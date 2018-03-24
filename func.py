e56dd1c05ac0176c8f733a2a514dc08f198067c6
ka10ryu1 <lsyp.tuiovjo.1231@gmail.com>
2018-03-24 13:25:13 +0900

    Squashed 'Tools/' content from commit 71c21b7
    git-subtree-dir: Tools
    git-subtree-split: 71c21b7fa12f118e9cae422d3091ae8f40a39001

---
+++ func.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+# -*-coding: utf-8 -*-
+#
+help = '便利機能'
+#
+
+import os
+import inspect
+from pathlib import Path
+from watchdog.events import FileSystemEventHandler
+
+
+class ChangeHandler(FileSystemEventHandler):
+
+    def __init__(self):
+        pass
+
+    def on_created(self, event):
+        filepath = event.src_path
+        filename = os.path.basename(filepath)
+        _, ext = os.path.splitext(filename)
+        return filepath, filename, ext
+
+    def on_modified(self, event):
+        filepath = event.src_path
+        filename = os.path.basename(filepath)
+        _, ext = os.path.splitext(filename)
+        return filepath, filename, ext
+
+    def on_deleted(self, event):
+        filepath = event.src_path
+        filename = os.path.basename(filepath)
+        _, ext = os.path.splitext(filename)
+        return filepath, filename, ext
+
+
+def argsPrint(p, bar=30):
+    """
+    argparseの parse_args() で生成されたオブジェクトを入力すると、
+    integersとaccumulateを自動で取得して表示する
+    [in] p: parse_args()で生成されたオブジェクト
+    [in] bar: 区切りのハイフンの数
+    """
+
+    print('-' * bar)
+    args = [(i, getattr(p, i)) for i in dir(p) if not '_' in i[0]]
+    for i, j in args:
+        if isinstance(j, list):
+            print('{0}[{1}]:'.format(i, len(j)))
+            [print('\t{}'.format(k)) for k in j]
+        else:
+            print('{0}:\t{1}'.format(i, j))
+
+    print('-' * bar)
+
+
+def checkModelType(path):
+    """
+    入力されたパスが.modelか.snapshotかそれ以外か判定し、
+    load_npzのpathを設定する
+    [in]  path:      入力されたパス
+    [out] load_path: load_npzのpath
+    """
+
+    # 拡張子を正とする
+    name, ext = os.path.splitext(os.path.basename(path))
+    load_path = ''
+    if(ext == '.model'):
+        print('model read:', path)
+    elif(ext == '.snapshot'):
+        print('snapshot read', path)
+        load_path = 'updater/model:main/'
+    else:
+        print('model read error')
+        print(fileFuncLine())
+        exit()
+
+    return load_path
+
+
+def getFilePath(folder, name, ext=''):
+    if not os.path.isdir(folder):
+        os.makedirs(folder)
+
+    return os.path.join(folder, name + ext)
+
+
+def sortTimeStamp(folder_list, ext):
+    path = []
+    [path.extend(list(Path(f).glob('*'+ext))) for f in folder_list]
+    return sorted([x.as_posix() for x in path], key=os.path.getmtime)
+
+
+def fileFuncLine():
+    """
+    この関数を呼び出すと、呼び出し先のファイル名、関数名、実行行数を取得できる
+    デバッグ時に便利
+    """
+
+    funcname = inspect.currentframe().f_back.f_code.co_name
+    filename = os.path.basename(
+        inspect.currentframe().f_back.f_code.co_filename
+    )
+    lineno = inspect.currentframe().f_back.f_lineno
+    return '>>> {0}, {1}(), {2}[line] <<<\n'.format(filename, funcname, lineno)
