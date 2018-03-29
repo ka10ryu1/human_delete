261160a0966becc2cd51a28b4d540d7337a37006
Ryuichi Kato <lsyp.tuiovjo.1231@gmail.com>
2018-03-29 10:49:18 +0900

    delete file and add img

--- clean_all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#clean_all.sh
-rm -rf ./*.npz ./result ./__pycache__/ ./*~ ./log.txt
-rm -rf ./Tools/__pycache__/ ./Tools/*~ ./Tools/result
-rm -rf ./Lib/__pycache__/ ./Lib/*~ ./Lib/result
-rm -rf ./Lib/Tests/__pycache__/ ./Lib/Tests/*~
